62712238a7f7d626dab1bf8c6de0162c1205c9da
0x414854 <0x414854@proton.me>
2025-10-20 15:51:46 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  console.log(token);
 
   // Si pas de token, redirige vers /login
   if (!token) {
